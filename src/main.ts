888e59eae4166beae49ceda4e425a1d22f39ceba
MrNizzy <mr.nizzy.0128@gmail.com>
2023-06-15 21:05:31 -0500

    Add: CORS Policy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const CORS: CorsOptions = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors(CORS);
   await app.listen(3000);
 }
 bootstrap();
